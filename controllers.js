381dab29ca29b4eaf7d07a62d5eb6c23be40bb98
intruston <intrustonly@gmail.com>
2023-01-25 15:40:43 +0100

    All done

--- controllers.js
+++ controllers.js
@@ -1,30 +1,72 @@
-const movies = require("./data");
-
-function getMovieById(id) {
-  const movie = movies.find(movie => movie.id === id);
-  if (!movie) {
-    throw new Error("Movie not found :(");
-  }
-  return movie;
-}
+let movies = require("./data");
 
 function getAllMovies() {
   return movies;
 }
 
-function addMovie(movie) {
+function getMovieById(id) {
+  const movie = movies.find((movie) => movie.id == id);
+  if (!movie) {
+    return { status: 404, message: "Movie is not found :(" };
+  }
+  return movie;
 }
 
-function updateMovie(id, newMovie) {
+function addMovie(req) {
+  if (isInvalid(req)) {
+    return { status: 400, message: "Invalid request" };
+  }
+  const lastId = movies.slice(-1)[0].id + 1;
+  const newMovie = {
+    id: lastId,
+    title: req.body.title,
+    director: req.body.director,
+    release_date: req.body.release_date,
+  };
+  movies.push(newMovie);
+  return { status: 201, id: lastId };
+}
+
+function updateMovie(id, req) {
+  if (isInvalid(req)) {
+    return { status: 400, message: "Invalid request" };
+  }
+  const movie = movies.find((movie) => movie.id == id);
+  if (!movie) {
+    return { status: 404, message: "Movie is not found :(" };
+  }
+  movie.title = req.body.title;
+  movie.director = req.body.director;
+  movie.release_date = req.body.release_date;
+  return { status: 200, message: `Movie with id: ${id} was updated` };
 }
 
 function deleteMovie(id) {
+  const movieToDelete = movies.find((movie) => movie.id === id);
+  if (!movieToDelete) {
+    return { status: 404, message: "Movie is not found :(" };
+  }
+  movies = movies.filter((movie) => movie.id !== id);
+  return { status: 200, message: "Movie was deleted." };
 }
 
+const isInvalid = (req) => {
+  if (
+    typeof req.body == "undefined" ||
+    typeof req.body.title == "undefined" ||
+    typeof req.body.director == "undefined" ||
+    typeof req.body.release_date == "undefined"
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 module.exports = {
-  getMovieById,
   getAllMovies,
+  getMovieById,
   addMovie,
   updateMovie,
-  deleteMovie
+  deleteMovie,
 };
--- index.js
+++ index.js
@@ -4,87 +4,48 @@ const port = 3000;
 
 app.use(express.json());
 
-//let movies = require("./data");
 const controllers = require("./controllers.js");
 
 app.get("/movie", (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.send(movies);
+  res.send(controllers.getAllMovies());
 });
 
 app.get("/movie/:id", (req, res) => {
   const id = Number(req.params.id);
-  const movie = movies.find((movie) => movie.id === id);
-  if (!movie) {
-    res.status(404).send("Movie is not found :(");
-    return;
-  }
+  const movie = controllers.getMovieById(id);
+  res.status(movie.status || 200);
   res.setHeader("Content-Type", "application/json");
   res.send(movie);
 });
 
 app.post("/movie", (req, res) => {
-  if (isInvalid(req)) {
-    res.status(400);
-    res.send("Invalid request");
-    return;
+  const added = controllers.addMovie(req);
+  res.status(added.status);
+  if (added.status === 201) {
+    res.send(`New movie was added with id: ${added.id}`);
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.send(added.message);
   }
-  const lastId = movies.slice(-1)[0].id + 1;
-  movies.push({
-    id: lastId,
-    title: req.body.title,
-    director: req.body.director,
-    release_date: req.body.release_date,
-  });
-  res.status(201);
-  res.send(`New movie was added with id: ${lastId}`);
 });
 
 app.put("/movie/:id", (req, res) => {
-  if (isInvalid(req)) {
-    res.status(400);
-    res.send("Invalid request");
-    return;
-  }
   const id = req.params.id;
-  const movie = movies.find((movie) => movie.id == id);
-  if (!movie) {
-    res.status(404);
-    res.send("Movie is not found :(");
-    return;
-  }
-  movie.title = req.body.title;
-  movie.director = req.body.director;
-  movie.release_date = req.body.release_date;
-  res.send(`Movie with id: ${id} was updated`);
+  const updated = controllers.updateMovie(id, req);
+  res.status(updated.status);
+  res.setHeader("Content-Type", "application/json");
+  res.send(updated.message);
 });
 
 app.delete("/movie/:id", (req, res) => {
   const id = Number(req.params.id);
-  const movieToDelete = movies.find((movie) => movie.id === id);
-  if (!movieToDelete) {
-    res.status(404);
-    res.send("Movie is not found :(");
-    return;
-  }
-  movies = movies.filter((movie) => movie.id !== id);
-  res.status(200);
-  res.send("Movie was deleted.");
+  const deleted = controllers.deleteMovie(id);
+  res.status(deleted.status);
+  res.setHeader("Content-Type", "application/json");
+  res.send(deleted.message);
 });
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
-const isInvalid = (req) => {
-  if (
-    typeof req.body == "undefined" ||
-    typeof req.body.title == "undefined" ||
-    typeof req.body.director == "undefined" ||
-    typeof req.body.release_date == "undefined"
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-};
